6474325072da974dcca19dcb383a22eb84394366
salman-abdulqadir <salmanabdulkadir82@gmail.com>
2022-12-29 19:49:09 +0400

    connection to database set

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,6 +2,13 @@ const allowedOrigins = require('./allowedOrigin')
 
 const corsOptions = {
     origin: (origin, callback) => {
-        
-    }
+        if(allowedOrigins.indexOf(origin) != -1 || !origin)
+            callback(null, true)
+        else
+            callback(new Error("Not allowed by CORS"), false)
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
 }
+
+module.exports = corsOptions
